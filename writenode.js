aa9c5837bfc99751520fdefe29ecca09c03fb80f
Angus <angustalves@gmail.com>
2016-10-20 03:06:25 +0000

    added python pip-init support

--- writenode.js
+++ writenode.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var obj = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-obj['engines'] = {"node":"^4.4.5"};
+if(process.argv[2]=='node') obj['engines'] = {"node":"^4.4.5"};
+else if(process.argv[2]=='python') obj['engines'] = {"python":"^2.7.6"};
 var str = JSON.stringify(obj,null,4);
 fs.writeFile('./package.json',str,'utf8');
